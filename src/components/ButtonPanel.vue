c36b786d6e67b7bf511edad749f4e2c62ecba139
Kirill <Lequeston@yandex.ru>
2020-03-26 15:29:38 +0300

    Кнопки перелистывания

---
+++ src/components/ButtonPanel.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+    div(class="button-panel")
+        a(href="#ourCourses" class="link")
+            button(class="button button-left" v-on:click="onClickLeftButton")
+        a(href="#ourCourses" class="link")
+            button(class="button button-right" v-on:click="onClickRightButton")
+</template>
+
+<script>
+    export default {
+        name: "ButtonPanel",
+        props: {
+            onClickRightButton: Function,
+            onClickLeftButton: Function,
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+    .button-panel
+        margin-top: 10px
+        display: flex
+        flex-direction: row
+        align-items: center
+        justify-content: center
+        align-content: center
+        width: 100%
+        height: auto
+        box-sizing: border-box
+        padding: 0 15% 0 15%
+
+        $width: 40px
+        .link
+            width: $width
+            height: $width
+            text-decoration: none
+
+            .button
+                display: flex
+                justify-content: center
+                align-items: center
+                background: inherit
+                border: 1px solid #424242
+                border-radius: 50%
+                font-size: $width
+                color: #424242
+                width: 100%
+                height: 100%
+
+            .button-right
+                margin-left: 20px
+
+            .button-left:after
+                content: '\003C'
+
+
+            .button-right:after
+                content: '\003E'
+</style>
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -2,33 +2,81 @@
     div(id="ourCourses" class="courses")
         div(class="title") {{title}}
         InformationBlock(
-            v-for="info in informationBlocks"
+            v-for="info in viewArray"
             :key="info.id"
             v-bind="getInfo(info)"
+        )
+        ButtonPanel(
+            :onClickRightButton="onClickRightButton"
+            :onClickLeftButton="onClickLeftButton"
         )
 </template>
 
 <script>
     import InformationBlock from "@/components/InformationBlock.vue";
+    import ButtonPanel from "@/components/ButtonPanel";
+
+    const DEFAULT_COUNT_VIEW = 4;
 
     export default {
         name: "Courses",
         props:{
             informationBlocks: Array,
         },
+        data(){
+            return{
+                title: 'Наши курсы',
+                visibleRange: {
+                    start: 0,
+                    end: DEFAULT_COUNT_VIEW,
+                }
+            }
+        },
+        computed:{
+            viewArray(){
+                let {start, end} = this.visibleRange;
+                let {informationBlocks} = this;
+                return informationBlocks.slice(start, end);
+            }
+        },
         methods: {
             getInfo(info){
                 let newInfo = {...info};
                 delete newInfo.id;
                 return newInfo;
-            }
-        },
-        data(){
-            return{
-                title: 'Наши курсы',
+            },
+            onClickRightButton(){
+                let {start, end} = this.visibleRange;
+                let length = this.informationBlocks.length;
+                if (start + DEFAULT_COUNT_VIEW > length){
+                    start = 0;
+                    end = DEFAULT_COUNT_VIEW;
+                } else {
+                    start = start + DEFAULT_COUNT_VIEW;
+                    end = (end + DEFAULT_COUNT_VIEW > length) ? length : (end + DEFAULT_COUNT_VIEW);
+                }
+                this.visibleRange.start = start;
+                this.visibleRange.end = end;
+            },
+            onClickLeftButton(){
+                let {start, end} = this.visibleRange;
+                let length = this.informationBlocks.length;
+                if (end - DEFAULT_COUNT_VIEW === 0){
+                    start = length - (length % DEFAULT_COUNT_VIEW);
+                    end = length;
+                } else if ((end - start) < DEFAULT_COUNT_VIEW){
+                    end = length - (length % DEFAULT_COUNT_VIEW);
+                    start = end - DEFAULT_COUNT_VIEW;
+                } else {
+                    end -= DEFAULT_COUNT_VIEW;
+                    start -= DEFAULT_COUNT_VIEW;
+                }
+                this.visibleRange.start = start;
+                this.visibleRange.end = end;
             }
         },
         components: {
+            ButtonPanel,
             InformationBlock,
         }
     }
